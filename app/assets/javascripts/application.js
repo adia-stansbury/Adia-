a963fc8bf57a912eed9a7e9b66123c68489186e8
Adia Stansbury <adia.stansbury@gmail.com>
2015-07-25 21:00:20 -0400

    Home pg done
    >

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require js/bootstrap
 //= require_tree .
-//= require bootstrap
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'homes#index'
   resources :abouts, only: [:index]
+  resources :runs, only: [:index]
+  resources :walks, only: [:index]
+  resources :foods, only: [:index]
 end
