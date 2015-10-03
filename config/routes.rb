1f8a720a6cf47f823b8647bb80bdca42e7c8fa32
Adia Stansbury <adia.stansbury@gmail.com>
2015-10-03 09:05:49 -0400

    Removes walk pg

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'homes#index'
   resources :abouts, only: [:index]
   resources :runs, only: [:index]
-  resources :walks, only: [:index]
   resources :foods, only: [:index]
 end
